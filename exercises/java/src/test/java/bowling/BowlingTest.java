323109469c09b1cdd5a217d28c6f0d76fc78f9b3
Jamie Gernon <jamie.gernon@autotrader.co.uk>
2025-05-14 13:43:13 +0100

    Add test for scoring a spare

--- exercises/java/src/test/java/bowling/BowlingTest.java
+++ exercises/java/src/test/java/bowling/BowlingTest.java
@@ -1,5 +1,6 @@
 package bowling;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 import java.util.List;
@@ -26,5 +27,15 @@ public class BowlingTest {
         int result = testSubject.acceptBowling(rolls);
 
         assertThat(result).isEqualTo(20);
+    }
+
+    @Test
+    @Ignore
+    public void WhenUserScoresASpare_shouldAddTheNextRollToTheFrame(){
+        List<Integer> rolls = List.of(1,9, 1,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0);
+
+        int result = testSubject.acceptBowling(rolls);
+
+        assertThat(result).isEqualTo(12);
     }
 }
