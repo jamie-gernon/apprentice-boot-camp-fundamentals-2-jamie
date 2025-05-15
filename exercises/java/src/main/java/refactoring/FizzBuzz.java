d69ed816811534a9d5d96876287648fcdb1fc31f
Jamie Gernon <jamie.gernon@autotrader.co.uk>
2025-05-15 13:58:31 +0100

    Extract variables for FizzBuzz

--- exercises/java/src/main/java/refactoring/FizzBuzz.java
+++ exercises/java/src/main/java/refactoring/FizzBuzz.java
@@ -6,28 +6,31 @@ import org.apache.commons.codec.binary.Hex;
 import java.nio.charset.StandardCharsets;
 
 class FizzBuzz {
+    private final int five = new int[]{0, 0, 0, 0, 0}.length;
 
     private int countsUpToOneHundred;
     private int countsUpToThree;
-    private int countsDownFromFive = new int[]{0, 0, 0, 0, 0}.length;
+    private int countsDownFromFive = five;
 
     String doFizzBuzzUpToOneHundred() {
         String resultString = "";
-        for (; countsUpToOneHundred < Byte.MAX_VALUE - 27; countsUpToOneHundred++) resultString += addFizzOrBuzz(countsUpToOneHundred) + " ";
+        int oneHundred = Byte.MAX_VALUE - 27;
+        for (; countsUpToOneHundred < oneHundred; countsUpToOneHundred++) resultString += addFizzOrBuzz(countsUpToOneHundred) + " ";
         return resultString.substring(0, resultString.length() - 1);
     }
 
     private String addFizzOrBuzz(int number) {
         countsUpToThree++;
         countsDownFromFive--;
-        String fizzOrBuzz = countsUpToThree == 0b11 || countsDownFromFive == 0 ? "" : String.valueOf(number + 1);
-        if (countsUpToThree == 0b11) fizzOrBuzz += fizz();
+        int three = 0b11;
+        String fizzOrBuzz = countsUpToThree == three || countsDownFromFive == 0 ? "" : String.valueOf(number + 1);
+        if (countsUpToThree == three) fizzOrBuzz += fizz();
         if (countsDownFromFive == 0) fizzOrBuzz += buzz();
         return fizzOrBuzz;
     }
 
     private String buzz() {
-        countsDownFromFive = new int[]{0, 0, 0, 0, 0}.length;
+        countsDownFromFive = five;
         try {
             return new String(Hex.decodeHex("42757a7a"), StandardCharsets.UTF_8);
         } catch (DecoderException e) {
