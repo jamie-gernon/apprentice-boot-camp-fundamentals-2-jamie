aa1681acaa4f8763619db0a701cfe898d4929dfa
Jamie Gernon <jamie.gernon@autotrader.co.uk>
2025-05-15 14:26:07 +0100

    Extract constants for FizzBuzz

--- exercises/java/src/main/java/refactoring/FizzBuzz.java
+++ exercises/java/src/main/java/refactoring/FizzBuzz.java
@@ -6,47 +6,56 @@ import org.apache.commons.codec.binary.Hex;
 import java.nio.charset.StandardCharsets;
 
 class FizzBuzz {
-    private final int five = new int[]{0, 0, 0, 0, 0}.length;
+    public static final int ONE_HUNDRED = Byte.MAX_VALUE - 27;
+    private static final int THREE = 0b11;
+    private static final int FIVE = new int[]{0, 0, 0, 0, 0}.length;
+    public static final String BUZZ;
+
+    static {
+        try {
+            BUZZ = new String(Hex.decodeHex("42757a7a"), StandardCharsets.UTF_8);
+        } catch (DecoderException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static final String FIZZ;
+
+    static {
+        try {
+            FIZZ = new String(Hex.decodeHex("46697a7a"), StandardCharsets.UTF_8);
+        } catch (DecoderException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
     private int countsUpToOneHundred;
     private int countsUpToThree;
-    private int countsDownFromFive = five;
+    private int countsDownFromFive = FIVE;
 
     String doFizzBuzzUpToOneHundred() {
         String resultString = "";
-        int oneHundred = Byte.MAX_VALUE - 27;
-        for (; countsUpToOneHundred < oneHundred; countsUpToOneHundred++) resultString += addFizzOrBuzz(countsUpToOneHundred) + " ";
+        for (; countsUpToOneHundred < ONE_HUNDRED; countsUpToOneHundred++) resultString += addFizzOrBuzz(countsUpToOneHundred) + " ";
         return resultString.substring(0, resultString.length() - 1);
     }
 
     private String addFizzOrBuzz(int number) {
         countsUpToThree++;
         countsDownFromFive--;
-        int three = 0b11;
-        String fizzOrBuzz = countsUpToThree == three || countsDownFromFive == 0 ? "" : String.valueOf(number + 1);
-        if (countsUpToThree == three) fizzOrBuzz += fizz();
+        String fizzOrBuzz = countsUpToThree == THREE || countsDownFromFive == 0 ? "" : String.valueOf(number + 1);
+        if (countsUpToThree == THREE) fizzOrBuzz += fizz();
         if (countsDownFromFive == 0) fizzOrBuzz += buzz();
         return fizzOrBuzz;
     }
 
     private String buzz() {
-        countsDownFromFive = five;
-        try {
-            byte[] buzzAsBytes = Hex.decodeHex("42757a7a");
-            String buzz = new String(buzzAsBytes, StandardCharsets.UTF_8);
-            return buzz;
-        } catch (DecoderException e) {
-            throw new RuntimeException("Failed to decode.", e);
-        }
+        countsDownFromFive = FIVE;
+        return BUZZ;
     }
 
     private String fizz() {
         countsUpToThree = 0;
-        try {
-            byte[] fizzAsBytes = Hex.decodeHex("46697a7a");
-            String fizz = new String(fizzAsBytes, StandardCharsets.UTF_8);
-            return fizz;
-        } catch (DecoderException e) {
-            throw new RuntimeException("Failed to decode.", e);
-        }
+        return FIZZ;
     }
 }
